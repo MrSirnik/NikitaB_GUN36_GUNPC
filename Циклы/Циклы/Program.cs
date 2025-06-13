6558b3e98ba02147bc3b6dbda1f069791571bdf3
piece_of_cake <nektotim21@gmail.com>
2025-06-13 10:30:13 +0300

    Creating the "Циклы" folder

---
+++ Циклы/Циклы/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Циклы
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Fibonaccy
+            int variable_exchange;
+            int number1 = 0;
+            int number2 = 1;
+            Console.WriteLine(number1);
+
+            for (int i = 0; i < 9;  i++)
+            {
+                Console.WriteLine(number2);
+                variable_exchange = number1;
+                number1 = number2;
+                number2 = variable_exchange + number2;
+            }
+
+            Console.WriteLine("-------------------------------------");
+            //Even numbers
+            
+            for (int i = 2; i < 21; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            Console.WriteLine("-------------------------------------");
+            //Multiplication table
+
+
+        }
+    }
+}
