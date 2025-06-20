a76b6e5c203fc52b9ba2e1d3f84c97c0055102da
MrSirnik <nektotim21@gmail.com>
2025-06-20 06:55:39 +0300

    the first version of classes

---
+++ Классы/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Классы
+{
+    class Unit
+    {
+        public string Name;
+        private float _Health;
+        public int Damage;
+        public float Armor;
+        public void Character_name(string name = "Unknown Unit")
+        {
+            Name = name;
+            _Health = 100f;
+            Damage = 5;
+            Armor = 0.6f;
+        }
+        public float GetRealHealth()
+        {
+            
+            return _Health * (1f + Armor);
+        }
+        public bool SetDamage( float value )
+        {
+            _Health = _Health - value * Armor;
+            if (_Health <= 0f) { return true; }
+            else { return false; }
+        }
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
