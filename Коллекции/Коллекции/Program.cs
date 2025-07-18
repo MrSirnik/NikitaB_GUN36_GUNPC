721ff05057179f1f4da98ae957b10ab6ae943684
piece_of_cake <nektotim21@gmail.com>
2025-07-18 18:21:24 +0300

    creature class ListTask

---
+++ Коллекции/Коллекции/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Коллекции
+{
+    internal class Program
+    {
+        private class ListTask
+        {
+            private List<string> listOfInts = new List<string> {"5", "8", "64"};
+            private string LOI;
+            public void TaskLoop()
+            {
+                while (true)
+                {
+                    Console.WriteLine("Введите '-exit' чтоб выйти");
+                    Console.Write("Введите значение для конца списка: ");
+                    LOI = Console.ReadLine();
+                    if (LOI == "-exit" || LOI == "-Exit")
+                    {
+                         break;
+                    }
+                    listOfInts.Add(LOI);
+
+                    for (int i = 0; i < listOfInts.Count; i++)
+                    {
+                        Console.WriteLine(listOfInts[i]);
+                    }
+
+                    Console.WriteLine("Введите '-exit' чтоб выйти");
+                    Console.Write("Введите значение для середины списка: ");
+                    LOI = Console.ReadLine();
+                    if (LOI == "-exit" || LOI == "-Exit")
+                    {
+                        break;
+                    }
+                    listOfInts.Add(LOI);
+
+                    for (int i = 0; i < listOfInts.Count; i++)
+                    {
+                        Console.WriteLine(listOfInts[i]);
+                    }
+                    listOfInts.Insert((int)(listOfInts.Count / 2), LOI);
+                }
+            }
+        }
+
+            private class LinkedListTask
+            {
+                private class Node { } // Узел списка
+
+                public void TaskLoop()
+                {
+                    // проверка ввода и вывод результата
+                }
+            }
+
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Enter 1,2 or 3 to check task 1,2 or 3");
+                int task = int.Parse(Console.ReadLine()); // Используйте tryParse
+                switch (task)
+                {
+                    case 1:
+                        var listTask = new ListTask();
+                        listTask.TaskLoop();
+                        break;
+                    case 2:
+                        
+                        break;
+                    case 3:
+                        
+                        break;
+            }
+            }
+        }
+    }
---
+++ Коллекции/Коллекции/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Коллекции")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Коллекции")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("af554f2e-e54d-4839-bcc4-e41e34c27596")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
