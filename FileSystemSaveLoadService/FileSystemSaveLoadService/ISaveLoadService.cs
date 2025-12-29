43a692e5b6d3585a2061360d77dfa498676abb71
MrSirnik <nektotim21@gmail.com>
2025-12-29 16:11:28 +0300

    partially Reading and loading data

---
+++ FileSystemSaveLoadService/FileSystemSaveLoadService/ISaveLoadService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileSystemSaveLoadService
+{
+    public interface ISaveLoadService<T>
+    {
+        void SaveData(T data, string identifier);
+
+        T LoadData(string identifier);
+    }
+}
